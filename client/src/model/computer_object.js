ba729037c628262ccaf844a1a3da747ad813b2f8
Alison Wood <alisonwood29@gmail.com>
2018-03-03 15:01:34 +0000

    added the computer object model constructor in computer_object.js

---
+++ client/src/model/computer_object.js
@@ -0,0 +1,10 @@
+const ComputerObject = function (date, object) {
+  id = object.id;
+  date = this.date;
+  name = object.attributes.title[0].value;
+  description1 = object.attributes.option.option1;
+  description2 = object.attributes.description[0].value;
+  image = object.attributes.multimedia[0].processed.large_thumbnail.location;
+  creator = object.attributes.lifecycle.creation[0].maker[0].summary_title;
+  type = object.attributes.name[0].value;
+}
