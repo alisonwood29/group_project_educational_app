bb4460f411cfd728b42628bf79a52e74bb16bb25
Alison Wood <alisonwood29@gmail.com>
2018-03-05 13:55:23 +0000

    changed computer object model to take in a date as second parameter

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -1,2 +1,185 @@
-!function(t){var e={};function n(o){if(e[o])return e[o].exports;var s=e[o]={i:o,l:!1,exports:{}};return t[o].call(s.exports,s,s.exports,n),s.l=!0,s.exports}n.m=t,n.c=e,n.d=function(t,e,o){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:o})},n.r=function(t){Object.defineProperty(t,"__esModule",{value:!0})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e,n){const o=n(2),s=n(1),i=function(t){console.log(t);const e=new s(t.data);console.log(e)};document.addEventListener("DOMContentLoaded",function(){const t=[];["co62748","co64128","co62427","co8359400","co503422","co8401352","co8035886","co8430789","co8184137","co8361071"].forEach(function(e){const n="http://collection.sciencemuseum.org.uk/objects/"+e;t.push(n)}),t.forEach(function(t){new o(t).get(i)})})},function(t,e){t.exports=function(t){id=t.id,name=t.attributes.title[0].value,description1=t.attributes.options.option1,description2=t.attributes.description[0].value,image=t.attributes.multimedia[0].processed.large_thumbnail.location,type=t.attributes.name[0].value}},function(t,e){const n=function(t){this.url=t};n.prototype.get=function(t){const e=new XMLHttpRequest;e.open("GET",this.url),e.setRequestHeader("accept","application/json"),e.addEventListener("load",function(){if(200!==this.status)return;const e=JSON.parse(this.responseText);t(e)}),e.send()},n.prototype.post=function(t,e){const n=new XMLHttpRequest;n.open("POST",this.url),n.setRequestHeader("Content-Type","application/json"),n.addEventListener("load",function(){if(201!==this.status)return;const e=JSON.parse(this.responseText);t(e)}),n.send(JSON.stringify(e))},t.exports=n}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Request = __webpack_require__(/*! ./model/request.js */ "./src/model/request.js");
+const ComputerObject = __webpack_require__(/*! ./model/computer_object.js */ "./src/model/computer_object.js");
+
+const app = function(){
+
+  const urlArray = [];
+  const baseURL = 'http://collection.sciencemuseum.org.uk/objects/'
+  const fixedComputerObjects = ["co62748", "co64128", "co62427",
+  "co8359400", "co503422", "co8401352", "co8035886",
+  "co8430789", "co8184137", "co8361071"];
+  fixedComputerObjects.forEach(function(computer){
+    const requestUrl = baseURL + computer;
+    urlArray.push(requestUrl);
+
+  });
+  // console.log(urlArray);
+
+
+  urlArray.forEach(function (url) {
+    const requestUrl = new Request (url);
+    requestUrl.get(computerAPIRequestComplete)
+  })
+
+}
+
+const computerAPIRequestComplete = function (computer) {
+  console.log(computer);
+  const computerObject = new ComputerObject(computer.data);
+  console.log(computerObject);
+}
+
+
+document.addEventListener('DOMContentLoaded', app);
+
+
+/***/ }),
+
+/***/ "./src/model/computer_object.js":
+/*!**************************************!*\
+  !*** ./src/model/computer_object.js ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+const ComputerObject = function (object, date) {
+  id = object.id;
+  date = this.date;
+  name = object.attributes.title[0].value;
+  description1 = object.attributes.options.option1;
+  description2 = object.attributes.description[0].value;
+  image = object.attributes.multimedia[0].processed.large_thumbnail.location;
+  // creator = object.attributes.lifecycle.creation[0].maker[0].summary_title;
+  type = object.attributes.name[0].value;
+}
+
+module.exports = ComputerObject;
+
+
+/***/ }),
+
+/***/ "./src/model/request.js":
+/*!******************************!*\
+  !*** ./src/model/request.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+const Request = function (url) {
+  this.url = url;
+}
+
+Request.prototype.get = function (callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', this.url);
+  request.setRequestHeader('accept', 'application/json');
+  request.addEventListener('load', function () {
+    if(this.status !== 200) return;
+
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  })
+  request.send();
+}
+
+Request.prototype.post = function (callback, body) {
+  const request = new XMLHttpRequest();
+  request.open('POST', this.url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.addEventListener('load', function () {
+    if(this.status !== 201) return;
+
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  })
+  request.send(JSON.stringify(body));
+
+}
+
+module.exports = Request;
+
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=bundle.js.map
--- client/src/model/computer_object.js
+++ client/src/model/computer_object.js
@@ -1,6 +1,6 @@
-const ComputerObject = function (object) {
+const ComputerObject = function (object, date) {
   id = object.id;
-  // date = this.date;
+  date = this.date;
   name = object.attributes.title[0].value;
   description1 = object.attributes.options.option1;
   description2 = object.attributes.description[0].value;
