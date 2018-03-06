8ff1a8e86ea39f07b5fbd07aef3971b56fa49ccb
Giuseppe <giuseppe.ibba@hotmail.com>
2018-03-06 15:17:17 +0000

    Added a canvas in idex.html

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -123,6 +123,7 @@ const getFromDBRequestComplete = function (computers) {
   });
   computerObjectView.sortByDate();
   console.log(computerObjectView);
+  computerObjectView.populateObjectDetails(computerObjectView.computerObjects[0]);
 }
 
 
@@ -191,24 +192,49 @@ module.exports = ComputerObject;
 /***/ (function(module, exports) {
 
 const ComputerObjectView = function () {
-  this.computerObjects = [];
+    this.computerObjects = [];
 }
 
 ComputerObjectView.prototype.addComputer = function (computer) {
-  this.computerObjects.push(computer);
+    this.computerObjects.push(computer);
 }
 
 ComputerObjectView.prototype.sortByDate = function () {
-  this.computerObjects.sort(function (earliest, latest) {
-    return earliest.date - latest.date;
-  });
+    this.computerObjects.sort(function (earliest, latest) {
+        return earliest.date - latest.date;
+    });
 }
 
 ComputerObjectView.prototype.clear = function () {
-  this.computerObjects = [];
+    this.computerObjects = [];
 }
 
+ComputerObjectView.prototype.populateObjectDetails = function (computer) {
+    const section = document.getElementById('computer-info');
+    const detailDiv = document.getElementById('computer-detail-info');
+    const imageDiv = document.getElementById('computer-detail-image');
+    const ul = document.createElement('ul');
+    detailDiv.appendChild(ul);
+    this.createListElement('Date: ', computer.date, ul);
+    this.createListElement('Name: ', computer.name, ul);
+    let description = computer.description1;
+    if (computer.description1 === undefined) {
+        description = computer.description2;
+    }
+    this.createListElement('Description: ', description, ul);
+    this.createListElement('Type: ', computer.type, ul);
+    const image = document.createElement('img');
+    image.src = computer.image;
+    imageDiv.appendChild(image);
 
+}
+
+ComputerObjectView.prototype.createListElement = function (label, text, surroundingDiv) {
+    const span = document.createElement('li');
+    span.innerText = label + text;
+    surroundingDiv.appendChild(span);
+
+}
 
 
 module.exports = ComputerObjectView;
